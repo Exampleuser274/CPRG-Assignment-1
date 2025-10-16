0cb511069f2693bfa0b2c391e5a9f9256532e691
Ryan Dezall <ryandezall@The-Black-Pearl.lan>
2025-10-16 13:43:01 -0600

    Finalizing code and txt file

--- grade_registry.py
+++ grade_registry.py
@@ -3,17 +3,17 @@
 # Ask to enter grades until the user types "-1"
 
 print("Welcome to the Grade Registry Program")
-continue_program = "yes"
+continue_program = input("Would you like to add a new student? y(yes),n(no))\n")
 grade = 0
-student_names = []
-gpas = []
+student_names = {}
+    
 
-while continue_program == "yes":
-    name = input("Please enter student name: \n")
-    sum =0
-    count =0
+while continue_program.lower() in ("yes", "y"):
+    name = input("Enter student's name; \n")
+    sum = 0
+    count = 0
     num = 0
-    print("Enter student GPA for each subject. Enter -1 to finish entering GPA.")
+    print("Enter student GPA for each subject. Enter -1 to stop entering GPA.")
 
     while num != -1:
         count += 1
@@ -25,21 +25,18 @@ while continue_program == "yes":
     else:
         gpa = 0
 
-    student_names.append(name)
-    gpas.append(gpa)
-    continue_program = input("Do you want to enter another student? Type yes or no: \n")
-    while continue_program != "yes" and continue_program != "no":
-        print("Please enter a valid response (yes or no)")
-        continue_program = input("Do you want to enter another student? Type yes or no: \n")
-    if continue_program == "no":
+    student_names[name] = gpa
+    continue_program = input("Would you like to add a new student? y(yes), n(no) \n")
+    while continue_program.lower() not in ("yes", "y") and continue_program.lower() not in ("no", "n"):
+        print("Incorrect Input, please enter y(yes), n(no)")
+        continue_program = input("Would you like to add a new student? y(yes), n(no) \n")
+    if continue_program.lower() in ("no", "n"):
         break
     
 
 print("This is the list of students in the system, and their corresponding accumulative GPA")
-for name, gpa in zip(student_names, gpas):
-    print(name, gpa)
-
-
+for name, gpa in student_names.items():
+    print(f"{name} {gpa:.2f}")
     
     
 '''if num > 4 or num < 0 and num != -1:
