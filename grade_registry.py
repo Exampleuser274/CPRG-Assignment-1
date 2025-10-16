5fa1cdbd4f2ffbe74f36204c70f07a6479d019f6
Ryan Dezall <ryandezall@The-Black-Pearl.lan>
2025-10-15 23:26:07 -0600

    changes

--- grade_registry.py
+++ grade_registry.py
@@ -1,28 +1,24 @@
 # We are trying to figure out the gpa for multiple students
 # Each student will have one name and multiple grades
-# Ask to enter grades until the user types "done"
+# Ask to enter grades until the user types "-1"
 
 print("Welcome to the Grade Registry Program")
-continue_program = "Yes"
+continue_program = "yes"
 grade = 0
 student_names = []
 gpas = []
 
-while continue_program == "Yes":
+while continue_program == "yes":
     name = input("Please enter student name: \n")
     sum =0
     count =0
     num = 0
-    print("Enter student GPA for each subject. Enter -1 to finish:")
+    print("Enter student GPA for each subject. Enter -1 to finish entering GPA.")
 
     while num != -1:
         count += 1
         sum += num
         num = float(input(""))
-        if num > 4 or num < 0 and num != -1:
-            print("Please enter a valid GPA between 0 and 4")
-            count -= 1
-            sum -= num
     if sum > 0:
         count -= 1
         gpa = sum / count
@@ -31,18 +27,22 @@ while continue_program == "Yes":
 
     student_names.append(name)
     gpas.append(gpa)
-    continue_program = input("Do you want to enter another student? Type Yes or No: \n")
-    while continue_program != "Yes" and continue_program != "No":
-        print("Please enter a valid response (Yes or No)")
-        continue_program = input("Do you want to enter another student? Type Yes or No: \n")
-    if continue_program == "No":
+    continue_program = input("Do you want to enter another student? Type yes or no: \n")
+    while continue_program != "yes" and continue_program != "no":
+        print("Please enter a valid response (yes or no)")
+        continue_program = input("Do you want to enter another student? Type yes or no: \n")
+    if continue_program == "no":
         break
     
 
+print("This is the list of students in the system, and their corresponding accumulative GPA")
 for name, gpa in zip(student_names, gpas):
     print(name, gpa)
 
 
     
     
-
+'''if num > 4 or num < 0 and num != -1:
+            print("Please enter a valid GPA between 0 and 4")
+            count -= 1
+            sum -= num'''
